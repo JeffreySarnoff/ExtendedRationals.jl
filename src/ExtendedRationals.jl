166d77266f1c4e4ba06f156575f89d2223ea857d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2026-03-16 17:21:44 -0400

    Update ExtendedRationals.jl

--- src/ExtendedRationals.jl
+++ src/ExtendedRationals.jl
@@ -1,5 +1,8 @@
 module ExtendedRationals
 
+export Qx32
+
 include("ExtendedRationalInt32s.jl") 
+const Qx32 = ExtendedRationals.ExtendedRationalInt32s.ExtendedRational32
 
 end # module ExtendedRationals
