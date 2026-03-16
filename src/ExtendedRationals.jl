a15b8a87b21e9c23e571254a4a0a521e8efead88
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2026-03-16 17:24:21 -0400

    Export Q32 along with Qx32 in ExtendedRationals

--- src/ExtendedRationals.jl
+++ src/ExtendedRationals.jl
@@ -1,8 +1,9 @@
 module ExtendedRationals
 
-export Qx32
+export Qx32, Q32
 
 include("ExtendedRationalInt32s.jl") 
 const Qx32 = ExtendedRationals.ExtendedRationalInt32s.ExtendedRational32
+const Q32 = ExtendedRationals.RationalInt32s.Rational32
 
 end # module ExtendedRationals
