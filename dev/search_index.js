f4ae396e5a94887f083c35be46fe8a72882722d0
Documenter.jl <documenter@juliadocs.github.io>
2026-03-16 21:10:54 +0000

    build based on 4ee938e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ExtendedRationals.jl","page":"Home","title":"ExtendedRationals.jl","text":"Extended rational number types for Julia.\n\n","category":"section"}]
+}
