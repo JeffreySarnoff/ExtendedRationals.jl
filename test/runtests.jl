f063e502c00fdfc95911641eb68faf13cd52d541
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2026-03-16 17:31:15 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,11 @@ using ExtendedRationals
 using Test
 using Aqua
 
-@testset "ExtendedRationals.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(ExtendedRationals)
-    end
-end
+include("RationalInt32s_tests.jl")
+include("ExtendedRationalInt32s_tests.jl")
+
+#@testset "ExtendedRationals.jl" begin
+#    @testset "Code quality (Aqua.jl)" begin
+#        Aqua.test_all(ExtendedRationals)
+#    end
+#end
